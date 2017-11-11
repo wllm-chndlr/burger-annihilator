2e05207b7e94834e1b505528cc897d3fd423609c
Chandler <chndlr.wllm@gmail.com>
2017-11-11 11:07:40 -0600

    new connect to antonio jawsdb

--- config/connection.js
+++ config/connection.js
@@ -19,18 +19,26 @@
 
 var mysql = require("mysql");
 
-var connection;
+// var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+// };
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "jemw3s9cg2piczpr",
+  password: "ky005pyp3iqz8kl7",
+  database: "mzi0qo1smzmryxf6"
 });
-};
 
 connection.connect(function (err) {
   if (err) {
